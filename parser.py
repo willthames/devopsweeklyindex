fb8d83c0bdb14afe77998488257851fbe9247cb2
Will Thames <will@musc.org.uk>
2012-05-23 21:05:22 +1000

    Initial commit

---
+++ parser.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+from urlparse import urlparse
+from contextlib import closing
+from collections import defaultdict
+import pprint
+import urllib2
+
+def segment(input):
+    soup = BeautifulSoup(input)
+ 
+    result = defaultdict(list)
+    
+    content = ""
+    for seg in soup.body.find_all(["p","h2"]):
+        if seg.name == "h2":
+            tag = seg.contents[0]
+        if seg.name == "p":
+            if seg.a:
+                url = seg.a.contents[0]
+                result[tag].append({'url': url, 'content': content})
+                content = ""
+            else:
+                content = content + "\n" + seg.contents[0]
+            
+    return result
+
+
+def read(resource):
+   pr = urlparse(resource) 
+
+   if pr.netloc: 
+       with closing(urllib2.urlopen(resource)) as fp:
+           html = fp.read()
+   else:
+       html = resource
+   return segment(html)
+
+if __name__ == "__main__":
+    # work on command line argument
+    result = read(sys.argv[1])
+    pprint.pprint(result)
