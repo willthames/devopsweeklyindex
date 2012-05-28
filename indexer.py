1b7ee31f86ecc7eaf4669c08018e115bfcdb3981
Will Thames <will@musc.org.uk>
2012-05-28 22:19:34 +1000

    First stab at indexing. Works well, but plenty of improvements to be made!

---
+++ indexer.py
@@ -0,0 +1,70 @@
+from whoosh.index import create_in, exists_in, open_dir
+from whoosh.fields import * 
+import parser
+import pprint
+
+
+def get_schema():
+    return Schema(id=ID,
+                  section=TEXT(stored=True),
+                  url=TEXT(stored=True), 
+                  content=TEXT(stored=True))
+
+
+def get_index():
+    """ Return an index
+    creates index if empty
+    
+    """ 
+    if not exists_in("index", indexname="contents"):
+        create_in("index", indexname="contents", schema=get_schema())
+    return open_dir("index", indexname="contents") 
+
+
+def check_for_presence(ix, segments):
+    """ Tests whether segments has already been indexed
+    Keyword arguments: 
+    ix: index
+    segments: dict with section keys of { url, content} pair arrays
+    
+    """
+    return False
+
+
+def add_to_index(writer, segment, section, resource):
+    """ Adds a segment to an index
+    Keyword arguments: 
+    writer: IndexWriter
+    segment: url, content pair
+    section: section of content
+    resouce: original resource of content
+    """
+    writer.add_document(id=unicode(resource),
+                        section=section,
+                        url=segment['url'],
+                        content=segment['content'])
+
+
+def index(resource, reindex=False):
+    segments = parser.read(resource)
+    
+    ix = get_index()
+    writer = ix.writer()
+    
+    if not check_for_presence(ix, segments) or reindex:
+        for title in segments:
+            for segment in segments[title]:
+                if segment["content"] and segment["url"]: 
+                    add_to_index(writer, segment, title, resource)
+    writer.commit()
+ 
+    return(ix)
+
+
+if __name__ == "__main__":
+    # work on command line argument
+    result = index(sys.argv[1])
+
+    with result.searcher() as searcher:
+        docnums = searcher.document_numbers()
+        pprint.pprint(searcher.key_terms(docnums, "content", numterms=1000))
